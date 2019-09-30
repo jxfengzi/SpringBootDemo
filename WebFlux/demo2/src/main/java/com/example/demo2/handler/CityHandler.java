77af2b8146311f8e4c504f1c2ab6ebef05ccc8eb
Ouyang Chengfeng <jxfengzi@gmail.com>
2019-09-30 09:12:07 +0800

    add demo2

---
+++ WebFlux/demo2/src/main/java/com/example/demo2/handler/CityHandler.java
@@ -0,0 +1,42 @@
+package com.example.demo2.handler;
+
+import com.example.demo2.dao.CityRepository;
+import com.example.demo2.domain.City;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author ouyang
+ */
+@Component
+public class CityHandler {
+
+    private final CityRepository cityRepository;
+
+    @Autowired
+    public CityHandler(CityRepository cityRepository) {
+        this.cityRepository = cityRepository;
+    }
+
+    public Mono<Long> save(City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.save(city)));
+    }
+
+    public Mono<City> findCityById(Long id) {
+        return Mono.justOrEmpty(cityRepository.findCityById(id));
+    }
+
+    public Flux<City> findAllCity() {
+        return Flux.fromIterable(cityRepository.findAll());
+    }
+
+    public Mono<Long> modifyCity(City city) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.updateCity(city)));
+    }
+
+    public Mono<Long> deleteCity(Long id) {
+        return Mono.create(cityMonoSink -> cityMonoSink.success(cityRepository.deleteCity(id)));
+    }
+}
